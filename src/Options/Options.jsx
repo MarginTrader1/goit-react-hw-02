b164d598aae38baf9b16e6199bd920627c7c1609
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-21 11:43:33 +0300

    Update Options.jsx

--- src/Options/Options.jsx
+++ src/Options/Options.jsx
@@ -4,25 +4,19 @@ const Options = ({ options, click, reset }) => {
   const { good, neutral, bad } = options;
 
   return (
-    <>
-      <div className={css.list}>
-        {Object.keys(options).map((item, index) => (
-          <button
-            key={index}
-            className={css.button}
-            onClick={() => click(item)}
-          >
-            {item}
-          </button>
-        ))}
+    <div className={css.list}>
+      {Object.keys(options).map((item, index) => (
+        <button key={index} className={css.button} onClick={() => click(item)}>
+          {item}
+        </button>
+      ))}
 
-        {(good > 0 || neutral > 0 || bad > 0) && (
-          <button className={css.button} onClick={() => reset()}>
-            {"reset"}
-          </button> //рендер кнопки по условию
-        )}
-      </div>
-    </>
+      {(good > 0 || neutral > 0 || bad > 0) && (
+        <button className={css.button} onClick={() => reset()}>
+          {"reset"}
+        </button> //рендер кнопки по условию
+      )}
+    </div>
   );
 };
 
