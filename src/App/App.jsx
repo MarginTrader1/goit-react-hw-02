dafc5115b0e3a97e10520928163e0d8ffbe03df4
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2024-06-21 10:33:52 +0300

    ДЗ без стилей

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,31 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import Description from "../Description/Description";
 import Options from "../Options/Options";
 import Feedback from "../Feedback/Feedback";
 import Notification from "../Notification/Notification";
 
-const initialState = {
+const initialState = { //начальный cтейт
   good: 0,
   neutral: 0,
   bad: 0,
 };
 
-const App = () => {
-  const [state, setState] = useState(initialState);
+const getStateFromLocalStorage = () => {
+  const state = JSON.parse(localStorage.getItem("feedback"));
+  return state === null ? initialState : state; //проверяем локал сторедж
 
+};
+
+const App = () => {
+  const [state, setState] = useState(getStateFromLocalStorage); //делаем стейт 
+
+  // функция изменения стейта 
   const updateFeedback = (feedbackType) => {
     setState({ ...state, [feedbackType]: state[feedbackType] + 1 });
   };
+
+  // функция сброса стейта
+  const resetState = () => {
+    setState(initialState)
+  }
+
   const { good, neutral, bad } = state;
 
   const totalFeedback = good + neutral + bad;
   const positive = Math.round((good / totalFeedback) * 100);
 
+  //сохраняем в локал сторедж
+  useEffect(() => {
+    localStorage.setItem("feedback", JSON.stringify(state));
+  }, [state]);
+
   return (
     <>
       <Description />
-      <Options options={state} click={updateFeedback} />
+      <Options options={state} click={updateFeedback} reset={resetState}/>
 
       {totalFeedback > 0 ? (
         <Feedback
--- src/Options/Options.jsx
+++ src/Options/Options.jsx
@@ -1,9 +1,19 @@
-const Options = ({ options, click }) => {
-  return Object.keys(options).map((item, index) => (
-    <button key={index} onClick={() => click(item)}>
-      {item}
-    </button>
-  ));
+const Options = ({ options, click, reset }) => {
+  const { good, neutral, bad } = options;
+
+  return (
+    <>
+      {Object.keys(options).map((item, index) => (
+        <button key={index} onClick={() => click(item)}>
+          {item}
+        </button>
+      ))}
+
+      {(good > 0 || neutral > 0 || bad > 0) && (
+        <button onClick={() => reset()}>{"reset"}</button>
+      )}
+    </>
+  );
 };
 
 export default Options;
