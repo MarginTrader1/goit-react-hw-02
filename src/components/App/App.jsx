a45ef373448cbac90288660ff4723daaca4f285f
Mykhailo Statsenko <66798324+MarginTrader1@users.noreply.github.com>
2025-10-20 16:00:27 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -25,7 +25,6 @@ const App = () => {
 
    // функция изменения стейта
    const updateFeedback = (feedbackType) => {
-      console.log(state.__proto__)
       setState((prevState) => {
          return { ...prevState, [feedbackType]: prevState[feedbackType] + 1 };
       });
